2c4a561ee116b9db1cd7465ee9fa167675657ff6
Кирилл Петашвили <kpetasvili@gmail.com>
2023-05-26 13:07:37 +0300

    test

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,7 +13,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 namespace ind.project.petashvili1
 {
     /// <summary>
@@ -33,6 +33,14 @@ namespace ind.project.petashvili1
         private void MinButton_MouseDown(object sender, MouseButtonEventArgs e)
         {
             this.WindowState = WindowState.Minimized;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (LeftGrid.Visibility == Visibility.Hidden)
+                LeftGrid.Visibility = Visibility.Visible;
+            else
+                LeftGrid.Visibility = Visibility.Hidden;
         }
     }
 }
